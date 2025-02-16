a9cc7d5128fb6dc06961eb267dba33c542079623
weiling <10936081+weiling59ling@user.noreply.gitee.com>
2025-02-16 23:26:52 +0800

    feat:增加路由以及tabbar

---
+++ src/router/index.ts
@@ -0,0 +1,31 @@
+import { createWebHistory, createRouter } from 'vue-router'
+
+import TabsView from '../views/tabs/TabsView.vue'
+import HomeView from '../views/tabs/components/HomeView.vue'
+import MineView from '../views/tabs/components/MineView.vue'
+import OrderView from '../views/tabs/components/OrderView.vue'
+
+const routes = [
+  { path: '/', component: TabsView },
+  { path: '/tabs', component: TabsView,children:[
+    {
+      path:'home',
+      component:HomeView,
+    },
+    {
+      path:'order',
+      component:OrderView,
+    },
+    {
+      path:'mine',
+      component:MineView,
+    },
+  ] 
+},
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+export default router
---
+++ src/views/tabs/TabsView.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+<router-view></router-view>
+        <van-tabbar v-model="active" @change="onChange">
+            <van-tabbar-item name="home" icon="home-o">首页</van-tabbar-item>
+            <van-tabbar-item name="order" icon="friends-o">订单</van-tabbar-item>
+            <van-tabbar-item name="mine" icon="setting-o">我的</van-tabbar-item>
+        </van-tabbar>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import {RouterView} from 'vue-router'
+import { useRoute,useRouter } from 'vue-router';
+const route = useRoute()
+const router = useRouter()
+const active = ref(route.name as string);
+function onChange(index:any){
+    router.push({
+        path:index
+    })
+}
+</script>
+
+<style scoped lang='scss'>
+</style>
